30cd2da42fe1a2149d2da7dad820b829ebebe599
Georgi Hristozov <georgi@forkbomb.nl>
2017-10-06 17:01:02 +0300

    Tidy up the code.

--- exercises/01/07fastpow.scm
+++ exercises/01/07fastpow.scm
@@ -1,21 +1,20 @@
 (load "../../lib/scm/unit.scm")
 
-(define (pow2 x)
-  (* x x))
+(define (square x) (* x x))
 
-(define (pow x n)
+(define (fastpow x n)
   (cond
     ((= n 0) 1)
     ; Викаме помощната процедура за вдигане на квадрат, защото нещо такова:
-    ; (* (pow x (/n 2)) (pow x (/ n 2)))
-    ; Би направило две извиквания към pow, от които няма смисъл.
-    ; Вместо pow2 може да се ползва и вградената процедура за степенуване - expt.
-    ((even? n) (pow2 (pow x (/ n 2))))
-    (else (* x (pow x (- n 1))))))
+    ; (* (fastpow x (/n 2)) (fastpow x (/ n 2)))
+    ; Би направило две извиквания към fastpow, от които няма смисъл.
+    ; Вместо fastpow2 може да се ползва и вградената процедура за степенуване - expt.
+    ((even? n) (square (fastpow x (/ n 2))))
+    (else (* x (fastpow x (- n 1))))))
 
-(assert= 1 (pow 2 0))
-(assert= 2 (pow 2 1))
-(assert= 4 (pow 2 2))
-(assert= 1024 (pow 2 10))
-(assert= 1024 (pow -2 10))
-(assert= -2048 (pow -2 11))
+(assert= 1 (fastpow 2 0))
+(assert= 2 (fastpow 2 1))
+(assert= 4 (fastpow 2 2))
+(assert= 1024 (fastpow 2 10))
+(assert= 1024 (fastpow -2 10))
+(assert= -2048 (fastpow -2 11))
